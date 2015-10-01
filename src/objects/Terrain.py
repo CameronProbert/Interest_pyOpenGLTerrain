9415530652d800770890b86fcccadeae51659723
probercame <c.probert@hotmail.co.nz>
2015-10-02 12:43:46 +1300

    Added Basic Terrain Class

---
+++ src/objects/Terrain.py
@@ -0,0 +1,36 @@
+'''
+Created on 2/10/2015
+
+@author: probercame
+'''
+
+class Terrain(object):
+    '''
+    classdocs
+    '''
+
+    MAX_X = 0
+    '''Width of the terrain'''
+    MAX_Y = 0
+    '''Height of the terrain'''
+    MAX_Z = 0
+    '''Depth of the terrain'''
+
+    points = {}
+
+
+    def generateTerrain(self):
+        pass
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        generateTerrain()
+
+
+    def toString(self):
+        print (MAX_X)
+        print (MAX_Y)
+        print (MAX_Z)
