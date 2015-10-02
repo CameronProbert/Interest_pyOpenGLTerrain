328dbbf8c26662227f669e81feca2ad6209b52cc
probercame <c.probert@hotmail.co.nz>
2015-10-02 13:05:29 +1300

    Started noise generation in terrain

--- src/objects/Terrain.py
+++ src/objects/Terrain.py
@@ -3,34 +3,45 @@ Created on 2/10/2015
 
 @author: probercame
 '''
+from random import randint
 
 class Terrain(object):
     '''
-    classdocs
+    The terrain object automatically generates terrain using fractals
     '''
 
-    MAX_X = 0
+    MAX_X = 128
     '''Width of the terrain'''
-    MAX_Y = 0
+    MAX_Y = 128
     '''Height of the terrain'''
-    MAX_Z = 0
+    MAX_Z = 128
     '''Depth of the terrain'''
 
     points = {}
-
+    noise = {}
 
     def generateTerrain(self):
-        pass
+        '''Generates the terrain and stores it in points'''
+        self.generateNoise()
 
+    def generateNoise(self):
+        '''Generates the noise map'''
+        indX = 0
+        while (indX < self.MAX_X):
+            indZ = 0
+            while (indX < self.MAX_X):
+                self.noise[indX][indZ] = randint(0,self.MAX_Y-1)
+                indZ+=1
+            indX+=1
 
     def __init__(self, params):
         '''
         Constructor
         '''
-        generateTerrain()
+        self.generateTerrain()
 
 
     def toString(self):
-        print (MAX_X)
-        print (MAX_Y)
-        print (MAX_Z)
+        print ('Max X: ' + str(self.MAX_X))
+        print ('Max Y: ' + str(self.MAX_Y))
+        print ('Max Z: ' + str(self.MAX_Z))
